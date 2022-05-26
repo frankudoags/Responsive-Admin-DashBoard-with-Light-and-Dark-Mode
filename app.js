0268e6fc7fed44e6adeb1c190583977f1b7dc088
Frank Udoags <frankudoags@gmail.com>
2022-05-26 18:05:21 +0100

    Fixes for bugs

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const openBtn = document.querySelector('#menu-btn'),
      closeBtn = document.querySelector('#close-btn'),
      sideMenu = document.querySelector('aside'),
-     themeToggler  = document.querySelector('.theme-toggler');
+     themeToggler  = document.querySelector('.theme-toggler'),
+     dashBoardAnchor = document.querySelectorAll('.anc');
+
+
+    const dataItems =  {
+        
+    };
+
+    dashBoardAnchor.forEach((anchor) => {
+        if(anchor.classList.contains('active')) {
+            dataItems.activeElem = anchor;
+        }
+    });
+
+
 
     //  Open MENU event listener
     openBtn.addEventListener('click', (e) => {
@@ -18,4 +32,16 @@ const openBtn = document.querySelector('#menu-btn'),
         document.body.classList.toggle('dark-theme-variables');
         themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
         themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+    })
+
+    // Set active class to clicked anchor tag
+    dashBoardAnchor.forEach( (anchor) => {
+        anchor.addEventListener('click', (e) => {
+            if(dataItems.activeElem === anchor) return;
+            
+            dataItems.activeElem.classList.remove('active');
+            dataItems.activeElem = e.currentTarget;
+            dataItems.activeElem.classList.add('active');
+    
+        })
     })
